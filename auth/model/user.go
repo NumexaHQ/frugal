ecee2d7d7d2c18cb9e550319ef951cfbb7d784ba
l0calh0st8080 <142974401+l0calh0st8080@users.noreply.github.com>
2023-09-20 01:39:49 +0530

    fix: fix auth

--- auth/model/user.go
+++ auth/model/user.go
@@ -1,15 +1,7 @@
 package model
 
-import (
-	"time"
-)
-
-type User struct {
-	ID             int64     `json:"id"`
-	Name           string    `json:"name"`
-	OrganizationID int32     `json:"organization_id"`
-	Email          string    `json:"email" validate:"required,email"`
-	Password       string    `json:"password" validate:"required,min=8"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+type RegisterUserRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
 }
